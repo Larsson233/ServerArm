dc5d4229d9ebae46b8dfa98c5d748fe923f53126
Larsson233 <oscarlarsson233@gmail.com>
2023-03-28 11:52:00 +0200

    kingkong1000

--- ARMdemo/create_ops_enviroment.sh
+++ ARMdemo/create_ops_enviroment.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-export location=northeurope
-export username=azureuser
-export projectName=AppServer
-
 echo "Enter the Resource Group name:" && read resourceGroupName \
 && echo "Enter the SSH public key:" && read key \
 && az group create --name $resourceGroupName --location "$location" \
 && az deployment group create --resource-group $resourceGroupName \
---template-file /c/Users/Oscar/Documents/Github/ServerArm/ARMdemo/deploy.json \
+--template-file /d/Kod/CLO22MinaGrejerNy/ServerArm/ARMdemo/deploy.json \
 --parameters projectName=$projectName adminUsername=$username adminPublicKey="$key" customData=@install_dotnet_runtime.sh customData2=@install_nginx_block.sh \
 && az vm show --resource-group $resourceGroupName --name "$projectName" --show-details --query publicIps --output tsv
