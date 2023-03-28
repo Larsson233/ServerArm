67bc690f6eeb90ce2310b2c059af3dba68b3baae
Larsson233 <oscarlarsson233@gmail.com>
2023-03-28 13:54:51 +0200

    pushittothelimit

--- ARMdemo/create_ops_enviroment.sh
+++ ARMdemo/create_ops_enviroment.sh
@@ -2,8 +2,8 @@
 
 echo "Enter the Resource Group name:" && read resourceGroupName \
 && echo "Enter the SSH public key:" && read key \
-&& az group create --name $resourceGroupName --location "$location" \
+&& az group create --name $resourceGroupName --location "northeurope" \
 && az deployment group create --resource-group $resourceGroupName \
 --template-file /d/Kod/CLO22MinaGrejerNy/ServerArm/ARMdemo/deploy.json \
---parameters projectName=$projectName adminUsername=$username adminPublicKey="$key" customData=@install_dotnet_runtime.sh customData2=@install_nginx_block.sh \
+--parameters projectName=AppServer adminUsername=azureuser adminPublicKey="$key" customData=@install_dotnet_runtime.sh customData2=@install_nginx_block.sh \
 && az vm show --resource-group $resourceGroupName --name "$projectName" --show-details --query publicIps --output tsv
--- ARMdemo/install_dotnet_runtime.sh
+++ ARMdemo/install_dotnet_runtime.sh
@@ -23,6 +23,6 @@ curl -o actions-runner-linux-x64-2.303.0.tar.gz -L https://github.com/actions/ru
 echo "e4a9fb7269c1a156eb5d5369232d0cd62e06bec2fd2b321600e85ac914a9cc73  actions-runner-linux-x64-2.303.0.tar.gz" | shasum -a 256 -c
 tar xzf ./actions-runner-linux-x64-2.303.0.tar.gz
 chown -R azureuser:azureuser /actions-runner
-su azureuser -c "./config.sh --url https://github.com/Larsson233/cicdrunner --token AY74E56VL5QKYZZVKRVPKXDEEK5NM --unattended"
+su azureuser -c "./config.sh --url https://github.com/Larsson233/cicdrunner --token AY74E57G4TKLAMID4UDE7CLEELP66 --unattended"
 ./svc.sh install
 ./svc.sh start
