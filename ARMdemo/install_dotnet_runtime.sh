6949f7d34699628ff6921192578c863e556c051d
Oscar Larsson <oscarlarsson233@gmail.com>
2023-03-28 11:11:21 +0200

    udpate

--- ARMdemo/install_dotnet_runtime.sh
+++ ARMdemo/install_dotnet_runtime.sh
@@ -3,3 +3,26 @@ wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.
 dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 apt-get update && apt-get install -y aspnetcore-runtime-6.0
+
+cat << EOF > /etc/systemd/system/MyApp.service
+[Unit]
+Description=CiCdDemo
+
+[Service]
+WorkingDirectory=/actions-runner/_work/cicdrunner/cicdrunner
+ExecStart= /usr/bin/dotnet /actions-runner/_work/cicdrunner/cicdrunner/cicdrunner.dll
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+
+mkdir actions-runner && cd actions-runner
+curl -o actions-runner-linux-x64-2.303.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.303.0/actions-runner-linux-x64-2.303.0.tar.gz
+echo "e4a9fb7269c1a156eb5d5369232d0cd62e06bec2fd2b321600e85ac914a9cc73  actions-runner-linux-x64-2.303.0.tar.gz" | shasum -a 256 -c
+tar xzf ./actions-runner-linux-x64-2.303.0.tar.gz
+chown -R azureuser:azureuser /actions-runner
+su azureuser -c "./config.sh --url https://github.com/Larsson233/cicdrunner --token AY74E56VL5QKYZZVKRVPKXDEEK5NM --unattended"
+./svc.sh install
+./svc.sh start
